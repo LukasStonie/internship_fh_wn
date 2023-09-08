030e301e4f88cc5f34a3e0daf8c64abe123a638a
Lukas Steininger <56015465+LukasStonie@users.noreply.github.com>
2023-09-08 13:36:58 +0200

    resolutions: restructure routes.py and make use of wtforms, add login required to all routes

--- reference_db/app/forms/forms.py
+++ reference_db/app/forms/forms.py
@@ -10,7 +10,7 @@ class SignupForm(FlaskForm):
     lastname = StringField('Nachname', validators=[InputRequired(), Length(min=2, max=60)])
     email = EmailField('E-Mail', validators=[InputRequired(), Length(min=2, max=60), Email()])
     password = PasswordField('Passwort', validators=[
-            InputRequired(),
+        InputRequired(),
         Length(min=8, max=60),
         EqualTo('password_repeat', message='Passwörter stimmen nicht überein')])
     password_repeat = PasswordField('Passwort wiederholen',
@@ -23,19 +23,29 @@ class LoginForm(FlaskForm):
 
 
 class LensesForm(FlaskForm):
-    zoom = IntegerField('Zoom', validators=[InputRequired(message="Bitte geben Sie eine Vergößerung ein") ])
-    numerical_aperture = IntegerField('Numerische Apertur', validators=[InputRequired(message="Bitte geben Sie eine numerische Apertur ein")])
+    zoom = IntegerField('Zoom', validators=[InputRequired(message="Bitte geben Sie eine Vergößerung ein")])
+    numerical_aperture = IntegerField('Numerische Apertur',
+                                      validators=[InputRequired(message="Bitte geben Sie eine numerische Apertur ein")])
+
 
 class LasersForm(FlaskForm):
-    wavelength = IntegerField('Wellenlänge [nm]', validators=[InputRequired(message='Bitte geben Sie eine Wellenlänge an')])
+    wavelength = IntegerField('Wellenlänge [nm]',
+                              validators=[InputRequired(message='Bitte geben Sie eine Wellenlänge an')])
+
 
 class AperturesForm(FlaskForm):
     size = IntegerField('Größe [µm]', validators=[InputRequired(message='Bitte geben Sie eine Größe an')])
+
 
 class SlidesForm(FlaskForm):
     name = StringField('Bezeichnung', validators=[InputRequired(message='Bitte geben Sie eine Bezeichnung an')])
 
 
 class SpectralRangesForm(FlaskForm):
-    start = IntegerField(label="Start [cm<sup>-1</sup>]", validators=[InputRequired(message='Bitte geben Sie einen Startwert an')])
+    start = IntegerField(label="Start [cm<sup>-1</sup>]",
+                         validators=[InputRequired(message='Bitte geben Sie einen Startwert an')])
     end = IntegerField('Ende [cm^-1]', validators=[InputRequired(message='Bitte geben Sie einen Endwert an')])
+
+
+class ResolutionsForm(FlaskForm):
+    description = StringField('Bezeichnung', validators=[InputRequired(message='Bitte geben Sie eine Bezeichnung an')])
--- reference_db/app/routes/resolutions/routes.py
+++ reference_db/app/routes/resolutions/routes.py
@@ -1,91 +1,79 @@
 import sqlalchemy.exc
 from flask import render_template, request, url_for, flash, redirect
+from flask_login import login_required
+
+from app.forms.forms import ResolutionsForm
 from app.routes.resolutions import bp
 from app.models.model import Resolution
 from app.extensions import db
 
 
-def validate_form(form):
-    # check if the form is valid
-    form_ok = True
-    # check every field if it is empty
-    # set form_ok to False if any field is empty
-    if not form['description']:
-        flash('Bezeichnung ist ein Pflichtfeld', 'error')
-        form_ok = False
-    # return whether form is valid
-    # return the values from the form for new slide or redirect
-    return form_ok, form['description']
-
-
 @bp.route('/')
+@login_required
 def index():
     resolutions = db.session.query(Resolution).all()
     return render_template('resources/resolutions/index.html', resolutions=resolutions)
 
 
-@bp.route('/new', methods=['GET', 'POST'])
+@bp.route('/new', methods=['GET'])
+@login_required
 def new():
-    # if the request method is POST, the form was submitted
-    # validate the form and create a new slide
-    if request.method == 'POST':
-        # validate form
-        # form_ok signifies if the form is valid or not
-        # description is the value from the form
-        form_ok, description = validate_form(request.form)
-        # if the form is not valid, redirect to the new page and pass the values from the form
-        if not form_ok:
-            return redirect(url_for('resolutions.new', description=description))
-        # if the form is valid, create a new slide and redirect to the index page
-        else:
-            # if unique constraint is violated, inform the user
-            try:
-                resolution = Resolution(description=description)
-                db.session.add(resolution)
-                db.session.commit()
-                return redirect(url_for('resolutions.index'))
-            except sqlalchemy.exc.IntegrityError:
-                flash('Diese Bezeichnung existiert bereits', 'error')
-                return redirect(url_for('resolutions.new', description=description))
-    # if the request method is GET, the user wants to display the form
+    form = ResolutionsForm()
+    return render_template('resources/resolutions/new.html', form=form)
+
+
+@bp.route('/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    # convert request.form to a form object
+    form = ResolutionsForm(request.form)
+    # if the form is not valid, redirect to the new page and pass the values from the form
+    if not form.validate():
+        return render_template('resources/resolutions/new.html', form=form)
+    # if the form is valid, create a new slide and redirect to the index page
     else:
-        return render_template('resources/resolutions/new.html', values=request.args)
+        # if unique constraint is violated, inform the user
+        try:
+            resolution = Resolution(description=form.description.data)
+            db.session.add(resolution)
+            db.session.commit()
+            return redirect(url_for('resolutions.index'))
+        except sqlalchemy.exc.IntegrityError:
+            flash('Diese Bezeichnung existiert bereits', 'error')
+            return render_template('resources/resolutions/new.html', form=form)
+
+
+@bp.route('/<resolution_id>/edit', methods=['GET'])
+@login_required
+def edit(resolution_id):
+    resolution = db.session.query(Resolution).filter_by(id=resolution_id).first()
+    form = ResolutionsForm(obj=resolution)
+    return render_template('resources/resolutions/edit.html', form=form)
 
 
 @bp.route('/<resolution_id>/edit', methods=['GET', 'POST'])
-def edit(resolution_id):
-    # if the request method is POST, the form was submitted
-    # validate the form and create a new slide
-    if request.method == 'POST':
-        # validate form
-        # form_ok signifies if the form is valid or not
-        # resolution_description is the value from the form
-        form_ok, description = validate_form(request.form)
-        # if the form is not valid, redirect to the new page and pass the values from the form
-        if not form_ok:
-            return redirect(url_for('resolutions.edit', resolution_id=resolution_id, description=description))
-        # if the form is valid, create a new slide and redirect to the index page
-        else:
-            # if unique constraint is violated, inform the user
-            try:
-                resolution = db.session.query(Resolution).filter_by(id=resolution_id).first()
-                resolution.description = description
-                db.session.commit()
-                return redirect(url_for('resolutions.index'))
-            except sqlalchemy.exc.IntegrityError:
-                flash('Diese Bezeichnung existiert bereits', 'error')
-                return redirect(url_for('resolutions.edit', description=description))
-    # if the request method is GET, the user wants to display the form
+@login_required
+def edit_post(resolution_id):
+    # convert request.form to a form object
+    form = ResolutionsForm(request.form)
+    # if the form is not valid, redirect to the new page and pass the values from the form
+    if not form.validate():
+        return render_template('resources/resolutions/edit.html', form=form)
+    # if the form is valid, create a new slide and redirect to the index page
     else:
-        args_len = len(request.args.keys())
-        resolution = vars(
-            db.session.query(Resolution).filter(Resolution.id == resolution_id).first()) \
-            if args_len == 0 \
-            else request.args
-        return render_template('resources/resolutions/edit.html', resolution=resolution)
+        # if unique constraint is violated, inform the user
+        try:
+            resolution = db.session.query(Resolution).filter_by(id=resolution_id).first()
+            resolution.description = form.description.data
+            db.session.commit()
+            return redirect(url_for('resolutions.index'))
+        except sqlalchemy.exc.IntegrityError:
+            flash('Diese Bezeichnung existiert bereits', 'error')
+            return render_template('resources/resolutions/edit.html', form=form)
 
 
 @bp.route('/<resolution_id>/delete', methods=['GET', 'POST'])
+@login_required
 def delete(resolution_id):
     resolution = db.session.query(Resolution).filter_by(id=resolution_id).first()
     db.session.delete(resolution)
