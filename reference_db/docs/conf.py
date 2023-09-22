3d5bf47d4971f6e5052120b2392c310cffe77885
Lukas Steininger <56015465+LukasStonie@users.noreply.github.com>
2023-09-22 08:34:13 +0200

    change conf.py

--- reference_db/docs/conf.py
+++ reference_db/docs/conf.py
@@ -32,7 +32,7 @@ release = '1.0.0'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
